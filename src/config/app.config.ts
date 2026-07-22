218557e0a5d180b047d640dacc106dfcac8550ef
Danh <danhnt.ce191720@gmail.com>
2026-07-22 11:34:39 +0700

    nothing

--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -6,7 +6,7 @@ dotenv.config({ path: path.join(__dirname, "../../.env") });
 export const appConfig = {
   port: parseInt(process.env.PORT ?? "3000", 10),
   nodeEnv: process.env.NODE_ENV ?? "development",
-  mongoUri: process.env.MONGO_URI ?? "mongodb://localhost:27017/eventsphere1",
+  mongoUri: process.env.MONGO_URI ?? "mongodb://localhost:27017/eventsphere",
   jwt: {
     accessSecret: process.env.JWT_ACCESS_SECRET ?? "dev_access_secret",
     accessExpires: process.env.JWT_ACCESS_EXPIRES ?? "15m",
